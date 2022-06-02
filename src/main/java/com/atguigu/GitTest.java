aa28a774220e77a023608919bea9c3741ff20016
zyn0211 <106643969+zyn0211@users.noreply.github.com>
2022-06-02 11:40:22 +0800

    GitHub上的修改

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,6 +3,6 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("新建");
-        System.out.println("GitHub上的修改");
+        System.out.println("GitHub上的修改！");
     }
 }
